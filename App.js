c200181ee466a9fbcf47da27cccad78fd103bcee
Dorian Buck <dorianbuck@gmail.com>
2021-10-29 15:00:01 +0200

    inutial setup

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import { List } from 'react-native-paper';
 
-export default function App() {
+const someData = {
+  data: [
+    { name: "Dorian", role: "Student" },
+    { name: "Loui", role: "Son" },
+    { name: "Someone", role: "Student" },
+  ],
+};
+
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>List of people</Text>
+      <FlatList
+        data={someData.data}
+        renderItem={({ item }) => {
+          return (
+            <List.Item>
+              `{item.name} is a {item.role}`
+            </List.Item>
+          );
+        }}
+        keyExtractor={( item ) => item.name}
+      />
+      <Text>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
